f45e2506c9508fea1b7bc841dfd91a56c66224f9
NEARworld <artemismars2@outlook.com>
2022-07-25 21:41:39 +0900

    context-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext, useState } from "react";
+import "./App.css";
+import { MyContext } from "./MyContext";
 
+function Child() {
+  const { name } = useContext(MyContext);
+  return <>{name}</>;
+}
+function Child2() {
+  const { name } = useContext(MyContext);
+  return <>{name}</>;
+}
 function App() {
+  const [name, setName] = useState("Sam");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyContext.Provider value={{ name, setName }}>
+        <Child />
+        <br />
+      </MyContext.Provider>
+      <Child2 />
     </div>
   );
 }
